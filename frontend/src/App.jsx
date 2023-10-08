0c9a3365b0691b876430f0f5d2b0c242b753b3da
Daniel Felipe Rosero Zemanate <dandan32099@gmail.com>
2023-10-08 16:27:32 -0500

    Adding frontend project structure and register form

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { RegisterForm } from "./components/RegisterForm"
+
+function App() {
+  return (
+    <>
+      <div>
+        <RegisterForm />
+      </div>
+    </>
+  )
+}
+
+export default App
