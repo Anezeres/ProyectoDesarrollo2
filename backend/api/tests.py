72849aa38e173694d3b96dbb0df2b6940f657dd9
jesusGomezCadena <jesus.cadena@correounivalle.edu.co>
2023-10-19 22:00:22 -0500

    ProducList (get) done

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
 
-# Create your tests here.
+class ProductosAPITest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_list_productos(self):
+        response = self.client.get('/api/productos/')
+        self.assertEqual(response.status_code, 200) 
