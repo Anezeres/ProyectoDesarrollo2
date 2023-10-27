a30714cf564709c08ab4256c49e10fa148454a1c
SuescunJhon <104960124+SuescunJhon@users.noreply.github.com>
2023-10-27 16:43:06 -0500

    Create views, serializers and validators for login (#5)
    Co-authored-by: Criser2013 <84862634+Criser2013@users.noreply.github.com>

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('productDetail/<int:pk>', views.ProductDetail.as_view(), name='productDetail'),
-    path('productCreate', views.ProductCreate.as_view(), name='productCreate'),
-    path('productos/', views.ProductoList.as_view(), name='producto-list')
+	path('register', views.UserRegister.as_view(), name='register'),
+	path('login', views.UserLogin.as_view(), name='login'),
+	path('logout', views.UserLogout.as_view(), name='logout'),
+	path('user', views.UserView.as_view(), name='user'),
+  path('productDetail/<int:pk>', views.ProductDetail.as_view(), name='productDetail'),
+  path('productCreate', views.ProductCreate.as_view(), name='productCreate')
+  path('productos/', views.ProductoList.as_view(), name='producto-list')
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,62 @@
-from .serializers import ProductSerializer
-from rest_framework import generics
 from api.models import Producto
+from django.contrib.auth import login, logout
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions, status
+from rest_framework import generics
 from rest_framework.permissions import IsAdminUser
+from .serializers import *
+
+
+# Create your views here.
+class UserRegister(APIView):
+	permission_classes = (permissions.AllowAny,)
+	def post(self, request):
+		serializer = UserRegisterSerializer(data=request.data)
+		if serializer.is_valid(raise_exception=True):
+			user = serializer.create(request.data)
+			if user:
+				return Response({"message": "Registered Successfully"}, status=status.HTTP_201_CREATED)
+		return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserLogin(APIView):
+	permission_classes = (permissions.AllowAny,)
+	authentication_classes = (SessionAuthentication,)
+	##
+	def post(self, request):
+		data = request.data
+		serializer = UserLoginSerializer(data=data)
+		if serializer.is_valid():
+			user = serializer.check_user(data)
+			login(request, user)
+			return Response({"message":"you're logged in"}, status=status.HTTP_200_OK)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserLogout(APIView):
+	permission_classes = (permissions.AllowAny,)
+	authentication_classes = ()
+	def post(self, request):
+		logout(request)
+		return Response({}, status=status.HTTP_200_OK)
+
+
+class UserView(APIView):
+	permission_classes = (permissions.IsAuthenticated,)
+	authentication_classes = (SessionAuthentication,)
+	##
+	def get(self, request):
+		serializer = UserSerializer(request.user)
+		return Response({'user': serializer.data}, status=status.HTTP_200_OK)
 
 
 class ProductoList(generics.ListCreateAPIView):
     queryset = Producto.objects.all()
     serializer_class = ProductSerializer
 
-   
+    
 class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
 	queryset = Producto.objects.all()
 	serializer_class = ProductSerializer
