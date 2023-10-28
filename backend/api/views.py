28814a70e3925253fe42c5f516fbcda4f482659c
SuescunJhon <104960124+SuescunJhon@users.noreply.github.com>
2023-10-28 16:11:53 -0500

    Update views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -15,10 +15,10 @@ class RegCliente(APIView):
 	
 	def post(self, request):
 		serializer = ClienteSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.create(serializer.validated_data)
-            return Response(status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+		if serializer.is_valid():
+			serializer.create(serializer.validated_data)
+			return Response(status=status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserLogin(APIView):
