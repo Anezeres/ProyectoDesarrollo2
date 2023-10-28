b0a275f06afcf7425121a231ca73ae20794ec488
SuescunJhon <104960124+SuescunJhon@users.noreply.github.com>
2023-10-28 16:29:51 -0500

    Update tests.py

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,11 +1,1 @@
-from django.test import TestCase
-from rest_framework.test import APIClient
-from api.models import *
 
-class ProductosAPITest(TestCase):
-    def setUp(self):
-        self.client = APIClient()
-
-    def test_list_productos(self):
-        response = self.client.get('/api/productos/')
-        self.assertEqual(response.status_code, 200) 
