bbd3316ce93d94ed2f6eb2918c4600aa797e2cb5
SuescunJhon <104960124+SuescunJhon@users.noreply.github.com>
2023-10-28 16:15:57 -0500

    Update urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,13 +3,13 @@ from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-  path('cliente/', RegCliente.as_view()),
-  path('login', views.UserLogin.as_view(), name='login'),
-  path('logout', views.UserLogout.as_view(), name='logout'),
-  path('user', views.UserView.as_view(), name='user'),
-  path('productDetail/<int:pk>', views.ProductDetail.as_view(), name='productDetail'),
-  path('productCreate', views.ProductCreate.as_view(), name='productCreate'),
-  path('productos/', views.ProductoList.as_view(), name='producto-list')
+	path('cliente/', views.RegCliente.as_view()),
+	path('login', views.UserLogin.as_view(), name='login'),
+	path('logout', views.UserLogout.as_view(), name='logout'),
+	path('user', views.UserView.as_view(), name='user'),
+	path('productDetail/<int:pk>', views.ProductDetail.as_view(), name='productDetail'),
+	path('productCreate', views.ProductCreate.as_view(), name='productCreate'),
+	path('productos/', views.ProductoList.as_view(), name='producto-list')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
