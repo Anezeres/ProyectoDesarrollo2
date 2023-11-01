47f84cb7713f868c5e129fbe76c65dd60a13cc7e
Cristian O. <84862634+Criser2013@users.noreply.github.com>
2023-11-01 17:19:08 -0500

    Model "Carrito" migrated.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
 	path('logout', views.UserLogout.as_view(), name='logout'),
 	path('user', views.UserView.as_view(), name='user'),
   path('productDetail/<int:pk>', views.ProductDetail.as_view(), name='productDetail'),
-  path('productCreate', views.ProductCreate.as_view(), name='productCreate')
+  path('productCreate', views.ProductCreate.as_view(), name='productCreate'),
   path('productos/', views.ProductoList.as_view(), name='producto-list')
 ]
