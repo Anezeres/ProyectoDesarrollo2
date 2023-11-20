e2f422eea199fabc7d94cf7fecbbaa83a4daa036
Anezeres <alvarez.julian@correounivalle.edu.co>
2023-11-20 17:16:07 -0500

    Se suben los cambios del frontend

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,23 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const LoginForm = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [responseMessage, setResponseMessage] = useState('');
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await fetch('http://127.0.0.1:8000/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email, password }),
+                credentials: 'include',
+            });
+
+            console.log("Hola mundo")
+
+            if (!response.ok) {
+                throw new Error(`Error en la solicitud: ${response.status}`);
+            }
+
+            const data = await response.json();
+            setResponseMessage(`Inicio de sesión exitoso: ${data.message}`);
+        } catch (error) {
+            setResponseMessage(`Error al iniciar sesión: ${error.message}`);
+        }
+    };
+
     return (
         <div className="h-full flex flex-col justify-center items-center">
             <h2 className="text-5xl mb-4 font-semibold">Iniciar Sesión</h2>
-            <form action="#" className="w-full max-w-xl">
+            <form action="#" className="w-full max-w-xl" onSubmit={handleFormSubmit}>
                 <p className="mb-4 text-xl">
                     Que bueno verte de nuevo, ¿Qué vamos a comprar hoy?
                 </p>
                 <div className="mt-5">
-                    <input type="email" placeholder="Correo electrónico" className="border border-gray-400 py-1 px-2 w-full text-2xl"/>
+                    <input value={email} onChange={(e) => setEmail(e.target.value)} required type="email" placeholder="Correo electrónico" className="border border-gray-400 py-1 px-2 w-full text-2xl" />
                 </div>
                 <div className="mt-5">
-                    <input type="password" placeholder="Contraseña" className="border border-gray-400 py-1 px-2 w-full text-2xl"/>
+                    <input value={password} onChange={(e) => setPassword(e.target.value)} required type="password" placeholder="Contraseña" className="border border-gray-400 py-1 px-2 w-full text-2xl" />
                 </div>
                 <div>
                     <button className="bg-amber-600 text-white w-full py-2 mt-5 text-center rounded-xl text-2xl font-semibold">Iniciar Sesión</button>
-                </div> 
+                </div>
             </form>
+
+            <p>{responseMessage}</p>
         </div>
     );
 }
--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -1,29 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const RegisterForm = () => {
+
+    const [email, setEmail] = useState('');
+    const [cedula, setCedula] = useState('');
+    const [telefono, setTelefono] = useState('');
+    const [nombre, setNombre] = useState('');
+    const [password, setPassword] = useState('');
+    const [resultadoConsulta, setResultadoConsulta] = useState('');
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        const requestData = {
+            email: email,
+            ced: cedula,
+            tel: telefono,
+            nombre: nombre,
+            password: password,
+        };
+
+
+        // Construye la URL con los parámetros proporcionados
+        const apiUrl = `http://127.0.0.1:8000/api/cliente/`;
+
+        try {
+            const response = await fetch(apiUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(requestData),
+            });
+
+            if (!response.ok) {
+                throw new Error(`Error en la solicitud: ${response.status}`);
+            }
+
+            const data = await response.json();
+            setResultadoConsulta(JSON.stringify(data));
+        } catch (error) {
+            setResultadoConsulta(`Error al realizar la consulta: ${error.message}`);
+        }
+    };
+
     return (
         <div className="h-full flex flex-col justify-center items-center">
             <h2 className="text-5xl mb-4 font-semibold">Registrarse</h2>
-            <form action="#" className="w-full max-w-xl">
+            <form action="#" className="w-full max-w-xl" onSubmit={handleFormSubmit}>
                 <p className="mb-4 text-xl">
                     Regístrate para poder acceder a todos los beneficios que tenemos para ti.
                 </p>
                 <div className="grid grid-cols-2 gap-4">
-                    <input type="text" placeholder="Nombre" className="border border-gray-400 py-1 px-2 text-2xl"/>
-                    <input type="text" placeholder="Apellido" className="border border-gray-400 py-1 px-2 text-2xl"/>
+                    <input value={nombre} onChange={(e) => setNombre(e.target.value)} type="text" placeholder="Nombre" className="border border-gray-400 py-1 px-2 text-2xl" />
+                    <input value={cedula} onChange={(e) => setCedula(e.target.value)} type="text" placeholder="Cedula" className="border border-gray-400 py-1 px-2 text-2xl" />
                 </div>
                 <div className="mt-5">
-                    <input type="email" placeholder="Correo electrónico" className="border border-gray-400 py-1 px-2 w-full text-2xl"/>
+                    <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Correo electrónico" className="border border-gray-400 py-1 px-2 w-full text-2xl" />
                 </div>
                 <div className="mt-5">
-                    <input type="password" placeholder="Contraseña" className="border border-gray-400 py-1 px-2 w-full text-2xl"/>
+                    <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Contraseña" className="border border-gray-400 py-1 px-2 w-full text-2xl" />
                 </div>
                 <div className="mt-5">
-                    <input type="password" placeholder="Confirmar Contraseña" className="border border-gray-400 py-1 px-2 w-full text-2xl"/>
+                    <input value={telefono} onChange={(e) => setTelefono(e.target.value)} type="text" placeholder="Telefono" className="border border-gray-400 py-1 px-2 w-full text-2xl" />
                 </div>
                 <div className="mt-5">
                     <label className="flex items-center">
-                        <input type="checkbox" className="border border-gray-400"/>
+                        <input type="checkbox" className="border border-gray-400" />
                         <span className="ml-2 text-xl">
                             He leído y acepto los <a href="#" className="text-amber-600 font-semibold">términos y condiciones de uso</a> y la <a href="#" className="text-amber-600 font-semibold">política de privacidad de datos personales</a>.
                         </span>
@@ -31,8 +74,9 @@ export const RegisterForm = () => {
                 </div>
                 <div>
                     <button className="bg-amber-600 text-white w-full py-2 mt-5 text-center rounded-xl text-2xl font-semibold">Registrarse</button>
-                </div> 
+                </div>
             </form>
+            {<p>{resultadoConsulta}</p>}
         </div>
     );
 }
