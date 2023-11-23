7e99653f3b96eb5f4f94e592ef58dd92d2f97d6b
Criser2013 <84862634+Criser2013@users.noreply.github.com>
2023-11-23 15:48:58 -0500

    Feature: U.S. 9 sales report - backend. (#14)
    * U.S. 9 finisehd.
    
    * Monthly sales report added.

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -5,6 +5,11 @@ admin.site.register(Producto)
 admin.site.register(Marca)
 admin.site.register(Categoria)
 admin.site.register(Carrito)
+admin.site.register(Factura_producto)
+admin.site.register(Factura)
+admin.site.register(Venta)
+admin.site.register(Talla)
+admin.site.register(Color)
 admin.site.register(Producto_img)
 # Register your models here.
 
