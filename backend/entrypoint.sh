face5a2129bb31f378abdd02d1c82ad5eaef11fd
Cristian O. <84862634+Criser2013@users.noreply.github.com>
2023-12-09 15:31:40 -0500

    Backend Dockerfile fixed.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,7 @@
-!/bin/sh
+#!/bin/sh
 
 python manage.py makemigrations
-python manage.py migrate --no-input
-python manage.py collectstatic --no-input
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
 
-gunicorn backend.wsgi:application --bind 0.0.0.0:${PORT}
+gunicorn backend.wsgi:application --bind 0.0.0.0:8000
